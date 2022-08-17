668c6a379693a323d21e0d68732bc98f39a0492b
alimohamedkurijan <109865837+alimohamedkurijan@users.noreply.github.com>
2022-08-17 10:05:35 -0700

    done

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+function BMI(weight, height ){
+
+let result = weight / (height * height)
+
+return  result ;
+
+} 
+
+console.log(BMI(1 , 1))
+
+function Status (bmi){
+
+    if ( bmi < 18.5){
+
+        return `وزنك نقص`
+
+    } else if ( bmi >=18.5 && bmi < 25){
+
+        return`وزنك صحي`
+    } else if( bmi >=25){
+
+        return`لديك زيادة في الوزن ` 
+    }
+}
+
+function calculate (){
+
+    let weight = document.getElementById(`weight`).value
+    let height = document.getElementById(`height`).value
+
+    let bmi = BMI(weight, height)
+
+    let desc = Status(bmi)
+
+    let div = document.getElementById(`result`)
+
+    
+
+    div.innerText = bmi + "==" + desc
+
+
+
+    
+
+
+}
+
+
+
 
